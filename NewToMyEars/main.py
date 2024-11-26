117bd7afc56a1df5bfa14f1451b18c6040ca8fdd
Leonardo Fernandez <147666585+Leokipo@users.noreply.github.com>
2024-11-26 16:53:20 -0500

    Finish getToken() function for access token

--- NewToMyEars/main.py
+++ NewToMyEars/main.py
@@ -1,14 +1,16 @@
 # Followed tutorial from YouTube on SpotifyAPI calls
-from dotenv import load_dotenv
-import os
 import base64
+from dotenv import load_dotenv
+import json
+import os
+from requests import post
 
 load_dotenv()
 
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
 
-def getToken(client_id, client_secret):
+def getToken():
     auth_string = client_id + ':' + client_secret
     auth_bytes = auth_string.encode('utf-8')
     auth_base64 = str(base64.b64encode(auth_bytes), 'utf-8')
@@ -19,3 +21,7 @@ def getToken(client_id, client_secret):
         "Content-Type": "application/x-www-form-urlencoded"
     }
     data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result['access_token']
+    return token
