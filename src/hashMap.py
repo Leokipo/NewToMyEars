1e51812a9e9d82cbab3bae6ebfabe5f06e89c3e7
kevinrhoads <kevin.rhoads@ufl.edu>
2024-11-26 14:53:30 -0500

    created a hash table ready for separate chaining

--- src/hashMap.py
+++ src/hashMap.py
@@ -3,3 +3,8 @@ class hashMap:
     def __init__(self, bucketCount, loadFactor):
         self.bucketCount = bucketCount
         self.loadFactor = loadFactor
+        '''
+        create an array of bucketCount buckets to store values
+        each bucket is its own array for collision resolution (separate chaining)
+        '''
+        self.hashTable = [[] for _ in range(bucketCount)]
