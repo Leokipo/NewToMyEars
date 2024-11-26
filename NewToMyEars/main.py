e2cf370ac538825dbfe52bd404875ae47f0c9877
Leonardo Fernandez <147666585+Leokipo@users.noreply.github.com>
2024-11-26 16:41:13 -0500

    Create getToken function for creating calls

--- NewToMyEars/main.py
+++ NewToMyEars/main.py
@@ -1,8 +1,21 @@
+# Followed tutorial from YouTube on SpotifyAPI calls
 from dotenv import load_dotenv
 import os
+import base64
 
 load_dotenv()
 
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
 
+def getToken(client_id, client_secret):
+    auth_string = client_id + ':' + client_secret
+    auth_bytes = auth_string.encode('utf-8')
+    auth_base64 = str(base64.b64encode(auth_bytes), 'utf-8')
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
