5e8c4ccc7cad1573ed840dd4e9e3f3eb858d4e32
liammcglothlin <liammcglothlin@ufl.edu>
2024-11-29 16:26:19 -0500

    Add min heap implementation

---
+++ minheap.py
@@ -0,0 +1,43 @@
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def heapify_up(self, index):
+        parent = (index - 1) // 2
+        while index > 0 and self.heap[index] < self.heap[parent]:
+            self.heap[index], self.heap[parent] = self.heap[parent], self.heap[index]
+            index = parent
+            parent = (index - 1) // 2
+
+    def heapify_down(self, index):
+        smallest = index
+        left = 2 * index + 1
+        right = 2 * index + 2
+
+        if left < len(self.heap) and self.heap[left] < self.heap[smallest]:
+            smallest = left
+        if right < len(self.heap) and self.heap[right] < self.heap[smallest]:
+            smallest = right
+
+        if smallest != index:
+            self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
+            self.heapify_down(smallest)
+
+    def insert(self, value):
+        self.heap.append(value)
+        self.heapify_up(len(self.heap) - 1)
+
+    def extract_min(self):
+        if len(self.heap) == 0:
+            raise IndexError("Heap is empty")
+        min_value = self.heap[0]
+        self.heap[0] = self.heap[-1]
+        self.heap.pop()
+        if self.heap:
+            self.heapify_down(0)
+        return min_value
+
+    def get_min(self):
+        if len(self.heap) == 0:
+            raise IndexError("Heap is empty")
+        return self.heap[0]
