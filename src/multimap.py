c0a31dc5f7f0d4a30e22577a7c6a8fa8723b731e
kevinrhoads <krhoads325@gmail.com>
2024-12-03 14:22:25 -0500

    Added multimap getSong function

--- src/multimap.py
+++ src/multimap.py
@@ -2,6 +2,9 @@
 Implementation of a multimap utilizing an array of sets.
 Each index of the array equates to a popularity index (0-100) and the set contains songs with that popularity index
 """
+
+import random
+
 class multimap:
     def __init__(self):
         self.map = [set() for _ in range(100)] # populate the map array with a set for each index
@@ -9,3 +12,8 @@ class multimap:
     def insert(self, index, songName):
         # insert a new song to the set at the corresponding popularity index
         self.map[index].add(songName)
+
+    def getSong(self, index):
+        # get a random song from the given popularity index
+        targetSet = self.map[index]
+        return random.choice(targetSet)
