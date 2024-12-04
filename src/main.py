03a3344803beb020d69505fede82694db2b36af7
kevinrhoads <krhoads325@gmail.com>
2024-12-04 14:47:52 -0500

    Created spotify login feature

--- src/main.py
+++ src/main.py
@@ -19,72 +19,9 @@ load_dotenv()
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
 redirect_uri = os.getenv('REDIRECT_URI')
-
-def authUser():
-    # followed tutorial by Imdad Codes https://www.youtube.com/watch?v=olY_2MW4Eik&t=90s
-    app = Flask(__name__)
-    app.secret_key = client_secret
-
-    AUTH_URL = 'https://accounts.spotify.com/authorize'
-    TOKEN_URL = 'https://accounts.spotify.com/api/token'
-    API_BASE_URL = 'https://api.spotify.com/v1/'
-
-    @app.route('/')
-    def index():
-        return "Welcome to NewToMyEars <a href='/login'>Login with Spotify</a>"
-
-    @app.route('/login')
-    def login():
-        scope = ["playlist-modify-private"]
-        params = {
-            'client_id': client_id,
-            'response_type': 'code',
-            'scope': scope,
-            'redirect_uri': redirect_uri,
-            'show_dialog': True
-        }
-
-        auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
-        return redirect(auth_url)
-
-    @app.route('/callback')
-    def callback():
-        if 'error' in request.args:
-            return jsonify({'error': request.args['error']})
-
-        if 'code' in request.args:
-            req_body = {
-                'code': request.args['code'],
-                'grant_type': 'authorization_code',
-                'redirect_uri': redirect_uri,
-                'client_id': client_id,
-                'client_secret': client_secret
-            }
-
-            response = requests.post(TOKEN_URL, data=req_body)
-            token_info = response.json()
-
-            session['access_token'] = token_info['access_token']
-            session['refresh_token'] = token_info['refresh_token']
-            session['expires_at'] = datetime.now().timestamp() + token_info['expires_in']
-
-            return redirect('/playlists')
-
-        @app.route('/playlists')
-        def make_playlist():
-            if 'access_token' not in session:
-                return redirect('/login')
-
-            if datetime.now().timestamp() > session['expires_at']:
-                return redirect('/refresh-token')
-
-            headers = {
-                'Authorization': f'Bearer {session['access_token']}',
-                'Content-Type': 'application/json'
-            }
-
-            response = requests.get(API_BASE_URL + 'me/playlists', headers=headers)
-            playlists = response.json()
+AUTH_URL = 'https://accounts.spotify.com/authorize'
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
+API_BASE_URL = 'https://api.spotify.com/v1/'
 
 def getToken():
     auth_string = client_id + ':' + client_secret
@@ -141,7 +78,100 @@ def getSongsofPlaylists(playlists):
             songs.append(song_pair)
     return songs
 
-spotify = authUser()
+"""
+Spotify user authentication
+"""
+# followed tutorial by Imdad Codes https://www.youtube.com/watch?v=olY_2MW4Eik&t=90s
+app = Flask(__name__)
+app.secret_key = client_secret
+
+@app.route('/')
+def index():
+    return "Welcome to NewToMyEars <a href='/login'>Login with Spotify</a>"
+
+@app.route('/login')
+def login():
+    scope = 'playlist-modify-private'
+    params = {
+        'client_id': client_id,
+        'response_type': 'code',
+        'scope': scope,
+        'redirect_uri': redirect_uri,
+        'show_dialog': True
+    }
+
+    auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
+    return redirect(auth_url)
+
+@app.route('/callback')
+def callback():
+    if 'error' in request.args:
+        return jsonify({'error': request.args['error']})
+
+    if 'code' in request.args:
+        req_body = {
+            'code': request.args['code'],
+            'grant_type': 'authorization_code',
+            'redirect_uri': redirect_uri,
+            'client_id': client_id,
+            'client_secret': client_secret
+        }
+
+        response = requests.post(TOKEN_URL, data=req_body)
+        token_info = response.json()
+
+        session['access_token'] = token_info['access_token']
+        session['refresh_token'] = token_info['refresh_token']
+        session['expires_at'] = datetime.now().timestamp() + token_info['expires_in']
+
+        return redirect('/playlists')
+
+    @app.route('/playlists')
+    def make_playlist():
+        if 'access_token' not in session:
+            return redirect('/login')
+
+        if datetime.now().timestamp() > session['expires_at']:
+            return redirect('/refresh-token')
+
+        headers = {
+            'Authorization': f"Bearer {session['access_token']}",
+            'Content-Type': 'application/json'
+        }
+
+        data = {
+            "name": "NewToMyEars",
+            "description": "A playlist of 20 unpopular Spotify songs",
+            "public": False,
+        }
+
+        response = requests.get(API_BASE_URL + 'me/playlists', headers=headers, data=data)
+        new_playlist = response.json()
+        return jsonify(new_playlist)
+
+    @app.route('/refresh-token')
+    def refresh_token():
+        if 'refresh_token' not in session:
+            return redirect('/login')
+
+        if datetime.now().timestamp() > session['expires_at']:
+            req_body = {
+                'grant_type': 'refresh_token',
+                'refresh_token': session['refresh_token'],
+                'client_id': client_id,
+                'client_secret': client_secret
+            }
+
+            response = requests.post(TOKEN_URL, data=req_body)
+            new_token_info = response.json()
+
+            session['access_token'] = new_token_info['access_token']
+            session['expires_at'] = datetime.now().timestamp() + token_info['expires_in']
+
+            return redirect('/playlists')
+
+app.run(host='0.0.0.0', debug=True)
+
 token = getToken()
 genre = input("Genre to explore: ")
 result = []
